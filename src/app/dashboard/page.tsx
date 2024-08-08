8755f7323011f00bac8ddd0468a60e48e9941502
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-08 13:30:05 -0300

    feat: add page dashboard inicial

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { Container } from '@/components/container';
 import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 const Dashboard = async () => {
@@ -8,8 +9,17 @@ const Dashboard = async () => {
   if (!session || !session.user) redirect('/');
 
   return (
-    <Container>
-      <h1>Dashboard</h1>
+    <Container classname="flex flex-col gap-6">
+      <div className="w-full flex items-center justify-between">
+        <h1>Meus Chamados</h1>
+        <Link
+          href="/dashboard/new"
+          className="px-5 py-2 rounded-lg bg-indigo-600 text-center text-neutral-50"
+        >
+          Cadastrar
+        </Link>
+      </div>
+      Tickets
     </Container>
   );
 };
