f54f067123ea33900fd4eecc5ceacf0ac50d20fe
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-07 21:05:42 -0300

    feat: add protecao de rotas privadas

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,17 @@
-import { Container } from '@/components/container'
+import { Container } from '@/components/container';
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 
+const Dashboard = async () => {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) redirect('/');
 
-const Dashboard = () => {
   return (
     <Container>
-        <h1>Dashboard</h1>
+      <h1>Dashboard</h1>
     </Container>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
